e3ed443c223e7affc7a605110acad7c0e021bdaa
fkdkdsj <32150887+fkdkdsj@users.noreply.github.com>
2018-02-24 16:34:04 +0800

    Update settings.py

--- testxx/settings.py
+++ testxx/settings.py
@@ -54,9 +54,9 @@ HTTPERROR_ALLOWED_CODES = [403, 400]
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'testxx.middlewares.TestxxDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'testxx.middlewares.TestxxDownloaderMiddleware': 543,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
